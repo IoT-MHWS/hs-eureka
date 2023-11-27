92100cb998104d610eff925ebf46ddb3c9b53960
Nikita Kulakov <nvkulakov.dev@gmail.com>
2023-11-27 18:29:53 +0300

    move from hs-ensemble

---
+++ src/main/java/artgallery/eureka/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package artgallery.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * EurekaServerApplication
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaServerApplication.class, args);
+  }
+}
